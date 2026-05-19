a0bc5c4d387d533a20f2ba2929413ec369a75a27
Paul <paul@theapplegates.com>
2026-05-18 23:32:46 -0400

    Update site configuration for AstroPaper

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -2,14 +2,14 @@ import { defineAstroPaperConfig } from "./src/types/config";
 
 export default defineAstroPaperConfig({
   site: {
-    url: "https://astro-paper.pages.dev/",
+    url: "https://paper.paulapplegate.com/",
     title: "AstroPaper",
     description: "A minimal, responsive and SEO-friendly Astro blog theme.",
-    author: "Sat Naing",
-    profile: "https://satna.ing",
+    author: "Paul Applegate",
+    profile: "https://paulapplegate.com",
     ogImage: "default-og.jpg",
     lang: "en",
-    timezone: "Asia/Bangkok",
+    timezone: "America/New_York",
     dir: "ltr",
   },
   posts: {
